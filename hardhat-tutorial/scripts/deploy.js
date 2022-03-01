89bd03673eb50481a2e74a646a1948cf8d593ddd
Umar <45851120+umarham@users.noreply.github.com>
2022-03-01 23:49:31 +0100

    Update deploy.js

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -4,7 +4,7 @@ async function main() {
   const whitelistContract = await ethers.getContractFactory("Whitelist");
 
   const deployedWhitelistContract = await whitelistContract.deploy(10);
-  await deployedWhitelistContract.deployed();
+  
 
   console.log("Whitelist Contract Address:",deployedWhitelistContract.address);
 
