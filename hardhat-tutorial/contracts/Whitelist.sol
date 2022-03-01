cd07896fefa5cabffd5ab3acc36bf69fb7b162b3
Umar <umarhameed943@gmail.com>
2022-03-01 22:54:25 +0100

    first commit

---
+++ hardhat-tutorial/contracts/Whitelist.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+
+    uint8 public maxWhitelistedAddresses;
+
+    uint8 public numAddressesWhitelisted;
+
+    mapping(address => bool) public whitelistedAddresses;
+
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses =  _maxWhitelistedAddresses;
+    }
+
+    function addAddressToWhitelist() public {
+        require(!whitelistedAddresses[msg.sender], "Sender has already been whitelisted");
+        require(numAddressesWhitelisted < maxWhitelistedAddresses, "More addresses cant be added, limit reached");
+        whitelistedAddresses[msg.sender] = true;
+        numAddressesWhitelisted += 1;
+    }
+
+}
